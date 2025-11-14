473f5734d54338518c4995910acc132828714e1f
Anurag raj <anuragraj6056@gmail.com>
2025-11-14 11:14:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -48,5 +48,9 @@ def decode():
         return send_file(buf, as_attachment=True, download_name="reconstructed.png", mimetype="image/png")
     return render_template("decode.html")
 
-if __name__=="__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
